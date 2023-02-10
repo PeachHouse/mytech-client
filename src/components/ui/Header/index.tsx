1b87b5b90f3d88a773bb8599caca6fa79dc5093e
Takehiro Arima <take@Takehiros-MacBook-Air.local>
2023-02-11 00:12:19 +0900

    transit to search page

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -3,6 +3,8 @@ import { css } from "@emotion/react";
 import { FC } from "react";
 import { RiArticleLine, AiOutlineSearch, TbLogout } from "react-icons/all"
 import { Notification } from "@/components/ui/Notification";
+import {useNavigate} from "react-router-dom";
+import {ROUTING_PATH} from "@/routes";
 
 const headerStyles = {
     container: css `
@@ -11,10 +13,18 @@ const headerStyles = {
     navigation: css `
       display: flex;
       justify-content: space-between;
+      align-items: center;
     `,
-    list: css `
+    wrapper: css `
       display: flex;
       gap: 15px;
+    `,
+    list: css `
+      cursor: pointer;
+      padding: 10px;
+      &:hover {
+        opacity: 0.5;
+      }
     `,
     icon: css `
       width: 70px;
@@ -22,18 +32,31 @@ const headerStyles = {
     `
 }
 
+type HeaderProps = {
+  onClick: () => void
+}
+
+const Presentation: FC<HeaderProps> = ({onClick}) => {
+  return  (
+    <header css={headerStyles.container}>
+      <nav css={headerStyles.navigation}>
+        <h2>mytech</h2>
+        <ul css={headerStyles.wrapper}>
+          <li css={headerStyles.list} onClick={onClick}><AiOutlineSearch size={22}/></li>
+          <li css={headerStyles.list}><Notification size={22}/></li>
+          <li css={headerStyles.list}><RiArticleLine size={22}/></li>
+          <li css={headerStyles.list}><TbLogout size={22}/></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
 export const Header: FC = () => {
-    return (
-        <header css={headerStyles.container}>
-            <nav css={headerStyles.navigation}>
-                <h2>mytech</h2>
-                <ul css={headerStyles.list}>
-                    <li><AiOutlineSearch size={22}/></li>
-                    <li><Notification size={22}/></li>
-                    <li><RiArticleLine size={22}/></li>
-                    <li><TbLogout size={22}/></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const navigate = useNavigate()
+
+  const handleTransitSearch = () => {
+    navigate(ROUTING_PATH.SEARCH)
+  }
+  return <Presentation onClick={handleTransitSearch}/>
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import { HomePage, LoginPage, NotFoundPage, SignupPage } from '@/pages'
+import { HomePage, LoginPage, NotFoundPage, SignupPage, SearchPage } from '@/pages'
 
 export const ROUTING_PATH = {
   HOME: '/',
   LOGIN: '/login',
-  SIGNUP: '/signup',
-  NOT_FOUND: '*',
+  SIGNUP: "/signup",
+  SEARCH: "/search",
+  NOT_FOUND: '*'
 }
 
 const router = createBrowserRouter([
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
     path: ROUTING_PATH.NOT_FOUND,
     element: <NotFoundPage />,
   },
+  {
+    path: ROUTING_PATH.SEARCH,
+    element: <SearchPage />,
+  }
 ])
 
 export const AppRoutes = () => {
