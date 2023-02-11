1fdd307f06b81fea6f3f7b2721aa9c53e066924d
peachbranch7 <peachbranch.0517@gmail.com>
2023-02-11 22:10:41 +0900

    refactor: 💡 remove unnecessary code

--- src/pages/Login/Logint.test.tsx
+++ src/pages/Login/Logint.test.tsx
@@ -1,11 +1,6 @@
 import { describe, it, expect } from 'vitest'
-import { LoginPage } from '@/pages/Login'
-import { render, screen } from '@testing-library/react'
 
 describe('something truthy and falsy', () => {
-  // render(<LoginPage />)
-  // screen.debug()
-
   it('true to be true', () => {
     expect(true).toBe(true)
   })
